e8615c5d15fc31555ec4edee26fdce83f73293c6
dimas <my@gmail.com>
2021-02-04 21:11:04 +0200

    add spatie/laravel-permission library

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+	      // My providers
+	      Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
